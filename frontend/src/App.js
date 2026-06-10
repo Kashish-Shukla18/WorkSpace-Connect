1460079b88aefabc84d20a45aa49d0f473358941
Kashish-Shukla18 <kashish.shukla.8076@gmail.com>
2026-06-10 16:01:09 +0530

    added redirection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
 import Dashboard from './Dashboard';
@@ -21,6 +21,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to={localStorage.getItem('token') ? "/dashboard" : "/login"} replace />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         
